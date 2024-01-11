68e36d43e8c97b630db0bbe70138ee383f3377c4
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-01-11 15:06:17 -0400

    feat: add category, color and notification in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,18 @@ import { EstadoDepartamento, departamento } from "./modulos/departamento";
 import { EstadoFuncionario, funcionario } from "./modulos/funcionario";
 import { EstadoMotivo, motivo } from "./modulos/motivo";
 import { EstadoRegistro, registro } from "./modulos/registro";
+import { category, EstadoCategory } from "./modulos/category";
+import { EstadoNotificacao, notificacao } from "./modulos/notificacao";
+import { color, EstadoColor } from "./modulos/cor";
 
 export interface Estado {
   funcionario: EstadoFuncionario;
   departamento: EstadoDepartamento;
   motivo: EstadoMotivo;
   registro: EstadoRegistro;
+  category: EstadoCategory;
+  notificacao: EstadoNotificacao;
+  color: EstadoColor;
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol();
@@ -28,12 +34,24 @@ export const store = createStore<Estado>({
     registro: {
       registros: [],
     },
+    category: {
+      categories: [],
+    },
+    notificacao: {
+      notificacoes: [],
+    },
+    color: {
+      cores: [],
+    },
   },
   modules: {
     departamento,
     funcionario,
     motivo,
     registro,
+    category,
+    notificacao,
+    color,
   },
 });
 
