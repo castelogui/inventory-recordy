d717f476d92096848a503d7a2a2994f828cd7dad
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-01-15 07:40:31 -0400

    feat: add api url

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 const apiClient: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8888",
+  baseURL: "https://backend-rh-brasilsuperatacado.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
